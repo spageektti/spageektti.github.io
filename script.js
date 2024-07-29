144074e7c7410c3294b1edd82c7ff99c701cf816
spageektti <git@spageektti.cc>
2024-07-29 11:10:27 +0200

    update script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+const options = {
+            strings: ['My name is Wiktor.', 'I am also known as spageektti.', 'I am Open Source maintainer and contributor.', 'I am a student.', 'I am a medalist of the Polish Junior Olympiad in Informatics.'
+            ],
+            typeSpeed: 60,
+            backSpeed: 60,
+            backDelay: 1000,
+            smartBackspace: false,
+            loop: true,
+        };
+
+        const typed = new Typed("#animated-heading", options);
