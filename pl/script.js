9e8829e86b776c3dfa9b6caa1a59ea78c5ad33d7
spageektti <git@spageektti.cc>
2024-07-29 15:29:15 +0200

    update pl/script.js

---
+++ pl/script.js
@@ -0,0 +1,12 @@
+const options = {
+            strings: ['Mam na imię Wiktor.', 'Jestem także znany jako spageektti.', 'Jestem opiekunem i kontrybutorem Open Source.', 'Jestem uczniem.', 'Jestem medalistą Olimpiady Informatycznej Juniorów.'
+            ],
+            typeSpeed: 60,
+            backSpeed: 60,
+            backDelay: 1000,
+            smartBackspace: false,
+            loop: true,
+            showCursor: false,
+        };
+
+        const typed = new Typed("#animated-heading", options);
